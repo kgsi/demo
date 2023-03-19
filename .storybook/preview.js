84766e676859a613c53c7c5f4f630697d5539719
shinichi.kogiso <kuusai1998@gmail.com>
2023-03-19 16:03:42 +0900

    Storybookの追加

---
+++ .storybook/preview.js
@@ -0,0 +1,17 @@
+import '../public/reset.css'
+import './global.css'
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+}
+
+export default preview
